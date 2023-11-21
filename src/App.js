68ed9fb3a3922c9af1b2ce2389700b726ebd4441
safa <safaboukhris@yahoo.fr>
2023-11-21 16:14:00 +0100

    adding

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Desert from './Desert';
+import Drink from './Drink';
+import Food from './Food';
+import Logo from './Logo';
+import Navbar from './Navbar';
+import Subject from './Subject';
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <Navbar/>
+      <Logo/>
+      <Subject/>
+      <Drink/>
+      <Food/>
+      <Desert/>
+
+    </div>
+  );
+}
+
+export default App;
